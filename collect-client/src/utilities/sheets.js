69f701f7c16ee21c26404f71c1219698a431bf4a
Carlos Alberto Pereira Gomes <carlos@apgomes.com>
2022-01-23 12:53:50 -0300

    style: update spreadsheet meta data

--- collect-client/src/utilities/sheets.js
+++ collect-client/src/utilities/sheets.js
@@ -1,15 +1,14 @@
-import {DateTime} from 'luxon';
+import { DateTime } from 'luxon';
 
-export default function sheets (procedures,user){
-
+export default function sheets(procedures, user) {
   const wb = window.XLSX.utils.book_new();
   wb.Props = {
-    Title: "Relatório de Produção",
-    Subject: "Produção HGRS",
+    Title: 'Relatório de Produção do Centro Cirúrgico',
+    Subject: 'Produção HGRS',
     Author: `${user.name}`,
     CreatedDate: DateTime.local().toISO(),
   };
-  wb.SheetNames.push("Sheet1");
+  wb.SheetNames.push('Sheet1');
   const header = [
     'Data',
     'Hora',
@@ -30,35 +29,32 @@ export default function sheets (procedures,user){
     'Usuário5',
     'Usuário6',
   ];
-  const rows = procedures.map((p)=>
-    [
-      DateTime.fromSQL(p.procDateTime).toFormat('dd/LL/yyyy'),
-      DateTime.fromSQL(p.procDateTime).toFormat('HH:mm'),
-      p.descr,
-      p.code,
-      p.execPlace,
-      p.ptName,
-      p.ptRecN,
-      p.ptAge,
-      p.ptGender,
-      p.ptWard,
-      p.ptBed,
-      p.team,
-      p.user1Name,
-      p.user2Name,
-      p.user3Name,
-      p.user4Name,
-      p.user5Name,
-      p.user6Name,
-    ] 
-  );
-  const wsData = [header,...rows];
+  const rows = procedures.map(p => [
+    DateTime.fromSQL(p.procDateTime).toFormat('dd/LL/yyyy'),
+    DateTime.fromSQL(p.procDateTime).toFormat('HH:mm'),
+    p.descr,
+    p.code,
+    p.execPlace,
+    p.ptName,
+    p.ptRecN,
+    p.ptAge,
+    p.ptGender,
+    p.ptWard,
+    p.ptBed,
+    p.team,
+    p.user1Name,
+    p.user2Name,
+    p.user3Name,
+    p.user4Name,
+    p.user5Name,
+    p.user6Name,
+  ]);
+  const wsData = [header, ...rows];
   // console.log(JSON.stringify(wsData,null,2));
   const ws = window.XLSX.utils.aoa_to_sheet(wsData);
   wb.Sheets.Sheet1 = ws;
-  const userName = user.name.replace(/\s/g,'');
+  const userName = user.name.replace(/\s/g, '');
   const dateTime = DateTime.local().toFormat('yyyyLLddHHmmss');
-  const fileName = `RelatorioProducao_${userName}_${dateTime}.xlsx`;
+  const fileName = `RelatorioProducaoCC_${userName}_${dateTime}.xlsx`;
   window.XLSX.writeFile(wb, fileName);
 }
-
